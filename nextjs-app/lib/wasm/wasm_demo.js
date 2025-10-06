30f2937af2226d2a61cdedfa646b0be13baca3fe
Nik Nasr <nik@restate.dev>
2025-10-06 14:41:57 +0100

    add nextjs
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ nextjs-app/lib/wasm/wasm_demo.js
@@ -0,0 +1,15 @@
+import * as imports from "./wasm_demo_bg.js";
+
+// switch between both syntax for node and for workerd
+import wkmod from "./wasm_demo_bg.wasm";
+import * as nodemod from "./wasm_demo_bg.wasm";
+if (typeof process !== "undefined" && process.release.name === "node") {
+  imports.__wbg_set_wasm(nodemod);
+} else {
+  const instance = new WebAssembly.Instance(wkmod, {
+    "./wasm_demo_bg.js": imports,
+  });
+  imports.__wbg_set_wasm(instance.exports);
+}
+
+export * from "./wasm_demo_bg.js";
