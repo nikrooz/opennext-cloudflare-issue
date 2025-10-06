bb4ea06f229b5dfbfca664fb514e8f3972ca986a
Nik Nasr <nik@restate.dev>
2025-10-06 14:42:50 +0100

    add worker
    Signed-off-by: Nik Nasr <nik@restate.dev>

---
+++ cloudflare-worker-app/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import { greet } from '@/lib/wasm/wasm_demo';
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		const message = greet('World');
+		return Response.json({ message });
+	},
+} satisfies ExportedHandler<Env>;
